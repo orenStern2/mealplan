f7c5d009f407a68ea49cb9d2ef2ee680a62fe1e1
oren <oren.stern2@gmail.com>
2024-08-23 09:37:39 +0300

    add the days

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleRecipe(id) {
+  var recipes = document.getElementsByClassName('recipe')
+  for (var i = 0; i < recipes.length; i++) {
+    if (recipes[i].id !== id) {
+      recipes[i].style.display = 'none'
+    }
+  }
+  var x = document.getElementById(id)
+  if (x) {
+    x.style.display = x.style.display === 'none' ? 'block' : 'none'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var coll = document.getElementsByClassName('collapsible')
+  for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+      this.classList.toggle('active')
+      var content = this.nextElementSibling
+      if (content.style.display === 'block') {
+        content.style.display = 'none'
+      } else {
+        content.style.display = 'block'
+      }
+    })
+  }
+})
